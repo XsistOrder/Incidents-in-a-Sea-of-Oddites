6a91b964e931f6648665cef91f847f60f0d14715
S201120058 <s201120058@ddsbstudent.ca>
2026-05-11 09:37:33 -0400

    added basic code for game class

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,23 @@
-public class Game {
+import javax.swing.*;
 
-    
+public class Game extends JPanel{
+    public Game(){
+
+    }
+    public void move() {
+
+    }
     public static void main(String[] args) throws InterruptedException{
+        JFrame frame = new JFrame("City Scape Assigment");
+        Game game = new Game();
 
+        frame.add(game);
+        frame.setSize(1000, 700);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        while (true)
+        {
+
+        }
     }
 }
